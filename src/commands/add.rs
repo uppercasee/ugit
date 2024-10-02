646e19e29d326d12e6e99512bfd93cd12ca44aca
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-10-02 09:39:49 +0545

    added to staging area

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -3,18 +3,13 @@ use anyhow::{Context, Result};
 use crate::{Index, IndexEntry};
 
 pub fn add_to_index(objectfile: String) -> Result<()> {
-    let index = Index::default();
+    let mut index = Index::default().read()?; // Read the index
 
-    let mut index = index.read()?;
-
-    let entry = IndexEntry::from_path(&objectfile)
+    let entry = IndexEntry::update_index(&objectfile)
         .with_context(|| format!("Failed to create index entry for file: {}", objectfile))?;
 
-    // Step 3: Add or update the entry in the index
-    index.remove_entry(&objectfile); // Ensure no duplicate entries
-    index.add_entries(vec![entry]);
+    index.add_entries(entry);
 
-    // Step 4: Write the updated index to the index file
     index
         .write()
         .with_context(|| "Failed to write updated index file")?;

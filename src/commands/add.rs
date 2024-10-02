37d68cb368b13a1c5db8b8c636c5cd257552eda5
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-10-02 10:28:46 +0545

    Add command needs some improvements

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -3,6 +3,10 @@ use anyhow::{Context, Result};
 use crate::{Index, IndexEntry};
 
 pub fn add_to_index(objectfile: String) -> Result<()> {
+    // TODO: Not optimized at all. We should make sure that
+    // there is any change in the file before updating the index.
+    // We just add the file no matter what.
+    
     let mut index = Index::default().read()?; // Read the index
 
     let entry = IndexEntry::update_index(&objectfile)
